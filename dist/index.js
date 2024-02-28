de23f0e44d82892e285851cecc15187bd5d99786
LaCovLoid <tnsgus0018@naver.com>
2024-02-29 07:58:32 +0900

    add function(rank) & function(favorite)

--- dist/index.js
+++ dist/index.js
@@ -75,6 +75,7 @@ async function userInfoHandler(req, res) {
   }
   if (user[0].favorite != null){
     favorite = user[0].favorite.split("/");
+    favorite.pop();
   }
   if (text !=""){
     result =  await connection.query("SELECT * FROM `words` WHERE "+text);
@@ -153,8 +154,71 @@ async function translateHandler(req, res) {
   }
 }
 
+app.get('/rank',rankHandler);
+async function rankHandler(req, res) {
+  if (connection == null) return;
+
+  let accessToken = req.headers["access-token"];
+  let score = req.query.score;
+  if (score == null) return;
+
+  if (accessToken) {
+    let [temp] = await connection.query("SELECT `user_id` FROM `tokens` WHERE `token`=?", [accessToken]);
+    if (temp.length == 0) return;
+    let [temp2] = await connection.query("SELECT `user_id` FROM `users` WHERE `id`=?", [temp[0].user_id]);
+    if (temp2.length == 0) return;
+    let userId = temp2[0].user_id;
+    await connection.query("INSERT INTO `rank`(`user_id`,`score`) VALUES(?,?)", [userId, score]);
+  }
+  let [result] = await connection.query("SELECT * FROM `rank` ORDER BY `score` DESC LIMIT 10");
+  let rank = [];
+  for (let i = 0; i < result.length; i++) {
+    rank.push({
+      user_id: result[i].user_id,
+      score: result[i].score,
+    });
+  }
+  res.send({rank:rank});
+}
+
+app.get('/modify_favorite',modifyFavoriteHandler);
+async function modifyFavoriteHandler(req,res) {
+  if (connection == null) return;
+
+  let wordId = req.query.wordId;
+  if (wordId == null || wordId == "") return;
+  let accessToken = req.headers["access-token"];
+  if (accessToken == null || accessToken == "") return;
+  
+  let [temp] = await connection.query("SELECT `user_id` FROM `tokens` WHERE `token`=?", [accessToken]);
+  if (temp.length == 0) return;
+  let [user] = await connection.query("SELECT `favorite` FROM `users` WHERE `id`=?", [temp[0].user_id]);
+  if (user.length == 0) {
+    await connection.query("UPDATE `users` SET `favorite`=? WHERE `id`=?", [wordId+"/",temp[0].user_id]);
+    res.send({success: true});
+    return;
+  }
+
+  let favorite = (user[0].favorite).split('/');
+  favorite.pop();
+  if (favorite.indexOf(wordId) > -1) {
+    favorite.splice(favorite.indexOf(wordId),1);
+  } else {
+    favorite.push(wordId);
+  }
+
+  let favoriteText = "";
+  for (let i = 0; i < favorite.length; i++) {
+    favoriteText += favorite[i] + '/';
+  }
+  await connection.query("UPDATE `users` SET `favorite`=? WHERE `id`=?", [favoriteText, temp[0].user_id]);
+
+}
+
 app.post('/login', loginHandler);
 async function loginHandler(req, res) {
+  if (connection == null) return;
+
   let id = req.body.id._value;
   let password = req.body.password._value;
   if (id == null || password == null || id == "" || password == "") {
--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import * as deepl from 'deepl-node';
 
 const app = express();
 const port = 3000;
-let connection: any;
+let connection:any;
 
 dotenv.config();
 connectServer();
@@ -33,7 +33,7 @@ async function connectServer() {
 }
 
 app.get('/random', randomHandler);
-async function randomHandler(req: any, res: any) {
+async function randomHandler(req:any, res:any) {
   if (connection == null) return;
   
   let [result] = await connection.query("SELECT * FROM `words` WHERE `yomi_word_same`='0' Order by rand() Limit 1");
@@ -44,7 +44,7 @@ async function randomHandler(req: any, res: any) {
 }
 
 app.get('/user_info', userInfoHandler);
-async function userInfoHandler(req: any, res: any) {
+async function userInfoHandler(req:any, res:any) {
   if (connection == null) return;
   
   let accessToken = req.headers["access-token"];
@@ -75,6 +75,7 @@ async function userInfoHandler(req: any, res: any) {
   }
   if (user[0].favorite != null){
     favorite = user[0].favorite.split("/");
+    favorite.pop();
   }
   if (text !=""){
     result =  await connection.query("SELECT * FROM `words` WHERE "+text);
@@ -83,7 +84,7 @@ async function userInfoHandler(req: any, res: any) {
 }
 
 app.get('/word_check', checkHandler);
-async function checkHandler(req: any, res: any) {
+async function checkHandler(req:any, res:any) {
   if (connection == null) return;
 
   let text = req.query.text;
@@ -109,7 +110,7 @@ async function checkHandler(req: any, res: any) {
 }
 
 app.get('/answer', answerHandler);
-async function answerHandler(req: any, res: any) {
+async function answerHandler(req:any, res:any) {
   if (connection == null) return;
 
   let wordId = req.query.wordId;
@@ -122,7 +123,7 @@ async function answerHandler(req: any, res: any) {
 }
 
 app.get('/translate', translateHandler);
-async function translateHandler(req: any, res: any):Promise<any> {
+async function translateHandler(req:any, res:any):Promise<any> {
   const authKey = String(process.env.authKey);
   const translator = new deepl.Translator(authKey);
 
@@ -153,8 +154,71 @@ async function translateHandler(req: any, res: any):Promise<any> {
   }
 }
 
+app.get('/rank',rankHandler);
+async function rankHandler(req:any, res:any) {
+  if (connection == null) return;
+
+  let accessToken = req.headers["access-token"];
+  let score = req.query.score;
+  if (score == null) return;
+
+  if (accessToken) {
+    let [temp] = await connection.query("SELECT `user_id` FROM `tokens` WHERE `token`=?", [accessToken]);
+    if (temp.length == 0) return;
+    let [temp2] = await connection.query("SELECT `user_id` FROM `users` WHERE `id`=?", [temp[0].user_id]);
+    if (temp2.length == 0) return;
+    let userId = temp2[0].user_id;
+    await connection.query("INSERT INTO `rank`(`user_id`,`score`) VALUES(?,?)", [userId, score]);
+  }
+  let [result] = await connection.query("SELECT * FROM `rank` ORDER BY `score` DESC LIMIT 10");
+  let rank:any = [];
+  for (let i = 0; i < result.length; i++) {
+    rank.push({
+      user_id: result[i].user_id,
+      score: result[i].score,
+    });
+  }
+  res.send({rank:rank});
+}
+
+app.get('/modify_favorite',modifyFavoriteHandler);
+async function modifyFavoriteHandler(req:any,res:any) {
+  if (connection == null) return;
+
+  let wordId = req.query.wordId;
+  if (wordId == null || wordId == "") return;
+  let accessToken = req.headers["access-token"];
+  if (accessToken == null || accessToken == "") return;
+  
+  let [temp] = await connection.query("SELECT `user_id` FROM `tokens` WHERE `token`=?", [accessToken]);
+  if (temp.length == 0) return;
+  let [user] = await connection.query("SELECT `favorite` FROM `users` WHERE `id`=?", [temp[0].user_id]);
+  if (user.length == 0) {
+    await connection.query("UPDATE `users` SET `favorite`=? WHERE `id`=?", [wordId+"/",temp[0].user_id]);
+    res.send({success: true});
+    return;
+  }
+
+  let favorite = (user[0].favorite).split('/');
+  favorite.pop();
+  if (favorite.indexOf(wordId) > -1) {
+    favorite.splice(favorite.indexOf(wordId),1);
+  } else {
+    favorite.push(wordId);
+  }
+
+  let favoriteText = "";
+  for (let i = 0; i < favorite.length; i++) {
+    favoriteText += favorite[i] + '/';
+  }
+  await connection.query("UPDATE `users` SET `favorite`=? WHERE `id`=?", [favoriteText, temp[0].user_id]);
+
+}
+
 app.post('/login', loginHandler);
-async function loginHandler(req: any, res:any) {
+async function loginHandler(req:any, res:any) {
+  if (connection == null) return;
+
   let id = req.body.id._value;
   let password = req.body.password._value;
   if (id == null || password == null || id == "" || password == "") {
@@ -177,7 +241,7 @@ async function loginHandler(req: any, res:any) {
 }
 
 app.post('/regist', registHandler);
-async function registHandler(req: any, res: any) {
+async function registHandler(req:any, res:any) {
   if (connection == null) return;
 
   let id = req.body.id._value;
